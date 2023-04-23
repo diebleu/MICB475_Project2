befc555a05c10dc2069688351b01152ed4b43f7a
Carly <66034945+diebleu@users.noreply.github.com>
2023-04-22 23:07:37 -0700

    Update CRP master.R

--- Documentation/CRP master.R
+++ Documentation/CRP master.R
@@ -677,3 +677,98 @@ isa_crp_12m$sign %>% rownames_to_column(var="ASV") %>%
   left_join(taxtable_12m_crp) %>% filter(p.value<0.05) %>% View()
 
 
+########## RELATIVE ABUNDANCE USING ISA 12M DATA ##
+                                            #ISA 6 months
+library(phyloseq)
+library(indicspecies)
+library(tidyverse)
+#Setting seed
+set.seed(1)
+
+#### Make list of datasets to iterate over ####
+
+# Dummy data, with different abundance cutoffs just for fun:
+comp = microbiome::transform(agp_infant_6m_final,'compositional')
+
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+
+#### Create function, set the default value of 'type' as AGP ####
+
+# We basically copy your whole code into here, change out agp_infant_6m_final for 'df', and we use 'type' to control parameters that are specific to AGP vs CRP analyses
+# I've also used ctrl+f to remove 'agp_' from the below code, just so that it's not confusing for others reading the code
+run_indicator_analysis = function(df, type = 'AGP'){
+  
+  # Set variable values that you can use to test the function, then comment these lines out once your code's ready to run
+  # df = agp_infant_6m_final; type = 'AGP'
+  
+  # infant_6m_final is changed to 'df'
+  genus_6m <- tax_glom(df, "Genus", NArm= FALSE)
+  genus_RA_6m <- transform_sample_counts(genus_6m, fun=function(x) x/sum(x))
+  
+  # The first example of using 'type' to define different code for AGP vs CRP
+  if(type=='AGP'){
+    isa_6m <- multipatt(t(otu_table(genus_RA_6m)), cluster = sample_data(genus_RA_6m)$agp_clin)
+  } else if (type=='CRP'){
+    isa_6m <- multipatt(t(otu_table(genus_RA_6m)), cluster = sample_data(genus_RA_6m)$crp_clin)
+  } else { # Juuust in case
+    print('ERROR - unknown "type"')
+  }
+  
+  taxtable_6m_agp <- tax_table(df) %>% as.data.frame() %>% rownames_to_column(var="ASV")
+  
+  isa_6m$sign = isa_6m$sign %>% rownames_to_column(var="ASV") %>%
+    left_join(taxtable_6m_agp) %>% filter(p.value<0.05)
+  
+  # INDICATOR SPECIES RELATIVE ABUNDANCE
+  
+  #6 MONTHS
+  # Had to change 6m.compositional to compositional.6m since names can't start with numbers
+  compositional.6m <- df %>% tax_glom('Genus') %>%  microbiome::transform("compositional")
+  
+  ISA_6m_psmelt <- psmelt(compositional.6m)
+  
+  #renaming OTU column to ASV
+  colnames(ISA_6m_psmelt)[1] = "ASV"
+  
+  ISA_6m_psmelt_sig <- ISA_6m_psmelt %>% filter(ASV %in% isa_6m$sign$ASV[isa_6m$sign$p.value<0.05])
+  
+  #Adding Pseudocount
+  ISA_6m_psmelt_sig1 = ISA_6m_psmelt_sig %>% mutate(Abundance = Abundance + min(.$Abundance[.$Abundance>0]))
+  
+  # Assign plot to 'p' so that we can save it to our output
+  # We need 'type' since it differs between analyses
+  if(type=='AGP'){
+    p = ISA_6m_psmelt_sig1 %>% 
+      ggplot(aes(Genus,Abundance,fill=agp_clin)) +
+      geom_boxplot() +
+      scale_y_log10(expand = expansion(mult = 0.1)) +
+      theme_classic(base_size = 16) +
+      scale_fill_discrete(labels=c('High', 'Low')) +
+      labs(fill='AGP Level') +
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+      ggpubr::stat_compare_means(aes(group=agp_clin),label='p.signif',size=6)
+  } else if(type=='CRP'){
+    p = ISA_6m_psmelt_sig1 %>% 
+      ggplot(aes(Genus,Abundance,fill=crp_)) +
+      geom_boxplot() +
+      scale_y_log10(expand = expansion(mult = 0.1)) +
+      theme_classic(base_size = 16) +
+      scale_fill_discrete(labels=c('High', 'Low')) +
+      labs(fill='CRP Level') +
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+      ggpubr::stat_compare_means(aes(group=crp_clin),label='p.signif',size=6)
+  } else {
+    print('Error = unknown "type"')
+  }
+  
+  # All the stuff we want to save in our output
+  return(list(ind_output = isa_6m, plot = p))
+}
+
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+
+#### Run the indicator code on each of your datasets of interest ####
+
+df.ind = run_indicator_analysis(agp_infant_6m_final,type='AGP')
+print(df.ind$plot)
+ggsave('isa_agp_6m.jpeg',height=6, width = 5)
